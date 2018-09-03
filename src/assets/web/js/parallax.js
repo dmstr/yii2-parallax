ddecd70ff4b8d3d1015af7e04613c6267448b7b3
German Bisurgi <gebisurgi@gmail.com>
2018-09-03 16:14:34 +0200

    Update parallax.js
    Corrected distance calculations

--- src/assets/web/js/parallax.js
+++ src/assets/web/js/parallax.js
@@ -14,13 +14,13 @@ window.requestParallaxFrame = function () {
   );
 }();
 
-var inViewport = function (e) {
-  var rect = e.getBoundingClientRect();
+var inViewport = function (rect) {
   return (rect.top + rect.height >= 0 && rect.bottom - rect.height <= window.innerHeight);
 };
 
 window.addEventListener('load', function () {
   var elements = Array.prototype.slice.call(document.querySelectorAll('[data-parallax]'));
+  console.log(elements);
   window.addEventListener('scroll', function () {
     window.requestParallaxFrame(function () {
       var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
@@ -28,9 +28,11 @@ window.addEventListener('load', function () {
         var attributes = e.getAttribute('data-parallax').replace(/\s/g,'').split(',');
         var x = Number(attributes[0]) || 0;
         var y = Number(attributes[1]) || 0;
-        var when = attributes[2] || 'visible';
-        var move = when === 'asap' ? scrollTop : (scrollTop - e.offsetTop + window.innerHeight);
-        if (inViewport(e)) {
+        var startCondition = attributes[2] || 'visible';
+        var rect = e.getBoundingClientRect();
+        var offsetTop = rect.top + scrollTop;
+        var move = startCondition === 'asap' ? scrollTop : (scrollTop - offsetTop + window.innerHeight);
+        if (inViewport(rect)) {
           e.setAttribute('style', '-webkit-transform: translate(' + move * x + 'px, ' + move * y + 'px); -ms-transform: translate(' + move * x + 'px, ' + move * y + 'px); transform: translate(' + move * x + 'px, ' + move * y + 'px);');
         }
       });
